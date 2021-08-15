37f0b45bf98fcdad0667ce76ceda091ef4eb6b4c
crazyrun123456 <464334456@qq.com>
2021-08-15 21:03:43 +0800

    commit 1

---
+++ src/main/java/com/huawei/Demo1.java
@@ -0,0 +1,13 @@
+package com.huawei;
+
+/**
+ * @CLassName: Demo1
+ * @Author： 46433
+ * @Date： 2021/8/15 21:02
+ * @Description TODO
+ */
+public class Demo1 {
+    public static void main(String[] args) {
+        System.out.println("hello git1");
+    }
+}
