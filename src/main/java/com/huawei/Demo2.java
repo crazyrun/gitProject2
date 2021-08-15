311b826e2d820a12058ea6c424975fcbb71508c8
crazyrun123456 <464334456@qq.com>
2021-08-15 21:11:46 +0800

    commit java

---
+++ src/main/java/com/huawei/Demo2.java
@@ -0,0 +1,13 @@
+package com.huawei;
+
+/**
+ * @CLassName: Demo2
+ * @Author： 46433
+ * @Date： 2021/8/15 21:10
+ * @Description TODO
+ */
+public class Demo2 {
+    public static void main(String[] args) {
+        System.out.println("java");
+    }
+}
